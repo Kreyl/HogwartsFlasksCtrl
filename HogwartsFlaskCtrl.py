0dd78e2cc06d1758151bf0ab9a77695f48ba5867
kreyl <laystoll@yandex.ru>
2023-10-12 01:16:45 +0300

    sheet read ok

--- HogwartsFlaskCtrl.py
+++ HogwartsFlaskCtrl.py
@@ -2,60 +2,54 @@ import time
 from GSheetReader import GSheetRdr
 from PyQt5 import QtWidgets
 from HogwartsFlasksUI import HogCtrlWindow
+import threading
 
-# GRdr = GSheetRdr()
-# GRdr.url = "https://docs.google.com/spreadsheets/d/15dcdgj4wC7PxBiPrd3yXb1rx570sCtQEPsK_vzpmmoA/edit?usp=sharing"
-
-# points_grif = -100000
-# points_slyze = -100000
-# points_rave = -100000
-# points_huff = -100000
+GRdr = GSheetRdr()
 
 
-# def TryConvert(txt: str):
-#     try:
-#         if txt.isnumeric():
-#             v = int(txt)
-#             if 0 <= v <= 255:
-#                 return v
-#     except ValueError:
-#         pass
-#     return None
-
-
-# GRdr.Connect()
-
-# while True:
-#     time.sleep(1)
-#     pg = GRdr.GetCellInt('A2')
-#     if pg is None:
-#         continue
-#     ps = GRdr.GetCellInt('B2')
-#     if ps is None:
-#         continue
-#     pr = GRdr.GetCellInt('C2')
-#     if pr is None:
-#         continue
-#     ph = GRdr.GetCellInt('D2')
-#     if ph is None:
-#         continue
-#
-#     # Check if changed
-#     if pg == points_grif and ps == points_slyze and pr == points_rave and ph == points_huff:
-#         continue
-#
-#     points_grif = pg
-#     points_slyze = ps
-#     points_rave = pr
-#     points_huff = ph
-#
-#     print(points_grif, points_slyze, points_rave, points_huff)
-
+def thd_auto_func(win: HogCtrlWindow):
+    was_en = False
+    points_grif = -100000
+    points_slyze = -100000
+    points_rave = -100000
+    points_huff = -100000
+    while True:
+        time.sleep(5) # No more than 60 read requests per user per minute
+        if win.cbSheetPollEn.isChecked():
+            if not was_en:
+                was_en = True
+                GRdr.url = win.edSheetUrl.text()
+                GRdr.Connect()
+            pg = GRdr.GetCellInt('A2')
+            if pg is None:
+                continue
+            ps = GRdr.GetCellInt('B2')
+            if ps is None:
+                continue
+            pr = GRdr.GetCellInt('C2')
+            if pr is None:
+                continue
+            ph = GRdr.GetCellInt('D2')
+            if ph is None:
+                continue
+            # Check if changed
+            if pg == points_grif and ps == points_slyze and pr == points_rave and ph == points_huff:
+                continue
+            points_grif = pg
+            points_slyze = ps
+            points_rave = pr
+            points_huff = ph
+            print(points_grif, points_slyze, points_rave, points_huff)
+            win.lblGrif.setText(str(points_grif))
+            win.lblSlyze.setText(str(points_slyze))
+            win.lblRave.setText(str(points_rave))
+            win.lblHuff.setText(str(points_huff))
 
 def main():
     app = QtWidgets.QApplication([])
     window = HogCtrlWindow()
     window.show()
+    threading.Thread(target=thd_auto_func, args=(window, )).start()
 
     # if uart.find_port():
     #     window.lblSta.setText("Device found at " + uart.ser.port)
--- HogwartsFlasksUI.py
+++ HogwartsFlasksUI.py
@@ -1,23 +1,107 @@
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QPushButton, QLabel, QLineEdit, QHBoxLayout, QVBoxLayout, QGridLayout, QMessageBox, \
-    QRadioButton, QFrame
+    QRadioButton, QFrame, QGroupBox, QCheckBox, QSpacerItem, QSizePolicy
 
+
+class QHLine(QFrame):
+    def __init__(self):
+        super(QHLine, self).__init__()
+        self.setFrameShape(QFrame.HLine)
+        self.setFrameShadow(QFrame.Sunken)
+
+
+class QVLine(QFrame):
+    def __init__(self):
+        super(QVLine, self).__init__()
+        self.setFrameShape(QFrame.VLine)
+        self.setFrameShadow(QFrame.Sunken)
 
 class HogCtrlWindow(QtWidgets.QWidget):
+    def OnModeChange(self):
+        if self.rbtnSheet.isChecked():
+            if not self.cbSheetPollEn.isEnabled():
+                self.cbSheetPollEn.setChecked(False)
+                self.cbSheetPollEn.setEnabled(True)
+                self.gbManual.setEnabled(False)
+        else:
+            self.cbSheetPollEn.setChecked(False)
+            self.cbSheetPollEn.setEnabled(False)
+            self.gbManual.setEnabled(True)
+
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Hogwarts Flasks Control")
+
         # Top switch
-        lblModeInfo = QLabel("Режим: автоматический из гуглотаблицы или ручной ввод")
-        self.rbtnSheet = QRadioButton("Из гуглотаблицы")
+        gb1 = QGroupBox("Режим")
+        self.rbtnSheet = QRadioButton("Автоматический из гуглотаблицы")
+        self.rbtnSheet.clicked.connect(self.OnModeChange)
         rbtnManual = QRadioButton("Ручной ввод")
         rbtnManual.setChecked(True)
+        rbtnManual.clicked.connect(self.OnModeChange)
         lt_Mode = QVBoxLayout()
-        lt_Mode.addWidget(lblModeInfo)
         lt_Mode.addWidget(self.rbtnSheet)
         lt_Mode.addWidget(rbtnManual)
+        gb1.setLayout(lt_Mode)
+
+        # Googlesheet settings
+        gb2 = QGroupBox("Настройки гуглотаблицы")
+        lblUrl = QLabel("URL таблицы:")
+        self.edSheetUrl = QLineEdit("https://docs.google.com/spreadsheets/d/15dcdgj4wC7PxBiPrd3yXb1rx570sCtQEPsK_vzpmmoA/edit?usp=sharing")
+        self.cbSheetPollEn = QCheckBox("Включить опрос")
+        self.cbSheetPollEn.setChecked(False)
+        self.cbSheetPollEn.setEnabled(False)
+        lt_gsheet = QGridLayout()
+        lt_gsheet.addWidget(lblUrl, 0, 0)
+        lt_gsheet.addWidget(self.edSheetUrl, 0, 1)
+        lt_gsheet.addWidget(self.cbSheetPollEn, 1, 0)
+        gb2.setLayout(lt_gsheet)
+
+        # Manual values
+        self.gbManual = QGroupBox("Ручной ввод баллов")
+        self.edGrif = QLineEdit("0")
+        self.edGrif.setMaximumSize(54, 20)
+        self.edSlyze = QLineEdit("0")
+        self.edSlyze.setMaximumSize(54, 20)
+        self.edRave = QLineEdit("0")
+        self.edRave.setMaximumSize(54, 20)
+        self.edHuff = QLineEdit("0")
+        self.edHuff.setMaximumSize(54, 20)
+        self.btnApply = QPushButton("Применить")
+        lt_Manual = QGridLayout()
+        lt_Manual.addWidget(QLabel('Введите нужные значения и нажмите "Применить"'), 0, 0, 1, 4)
+        lt_Manual.addWidget(QLabel("Гриффиндор"), 1, 0)
+        lt_Manual.addWidget(QLabel("Слизерин"), 1, 1)
+        lt_Manual.addWidget(QLabel("Рэйвенкло"), 1, 2)
+        lt_Manual.addWidget(QLabel("Хаффлпафф"), 1, 3)
+        lt_Manual.addWidget(self.edGrif, 2, 0)
+        lt_Manual.addWidget(self.edSlyze, 2, 1)
+        lt_Manual.addWidget(self.edRave, 2, 2)
+        lt_Manual.addWidget(self.edHuff, 2, 3)
+        lt_Manual.addWidget(self.btnApply, 3, 0, 1, 4)
+        self.gbManual.setLayout(lt_Manual)
+
+        # Current values
+        gb4 = QGroupBox("Текущие значения баллов")
+        self.lblGrif = QLabel("0")
+        self.lblSlyze = QLabel("0")
+        self.lblRave = QLabel("0")
+        self.lblHuff = QLabel("0")
+        lt_CurVal = QGridLayout()
+        lt_CurVal.addWidget(QLabel("Гриффиндор"), 0, 0)
+        lt_CurVal.addWidget(QLabel("Слизерин"), 0, 1)
+        lt_CurVal.addWidget(QLabel("Рэйвенкло"), 0, 2)
+        lt_CurVal.addWidget(QLabel("Хаффлпафф"), 0, 3)
+        lt_CurVal.addWidget(self.lblGrif, 1, 0)
+        lt_CurVal.addWidget(self.lblSlyze, 1, 1)
+        lt_CurVal.addWidget(self.lblRave, 1, 2)
+        lt_CurVal.addWidget(self.lblHuff, 1, 3)
+        gb4.setLayout(lt_CurVal)
 
         # Global layout
         lt_main = QVBoxLayout()
-        lt_main.addLayout(lt_Mode)
+        lt_main.addWidget(gb1)
+        lt_main.addWidget(gb2)
+        lt_main.addWidget(self.gbManual)
+        lt_main.addWidget(gb4)
         self.setLayout(lt_main)
